12d569db9f53d42211fbb5a31028d22abdb1d4dd
Marek Nalepa <marek.nalepa@gmail.com>
2015-08-26 15:25:16 +0200

    Created basic packages and main classes.

---
+++ FuelDataGenerator/src/pl/polsl/tpdia/fueldata/generator/FuelDataGenerator.java
@@ -0,0 +1,17 @@
+package pl.polsl.tpdia.fueldata.generator;
+
+/**
+ * @author mnalepa
+ *
+ */
+public class FuelDataGenerator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ FuelDataProcessor/src/pl/polsl/tpdia/fueldata/processor/FuelDataProcessor.java
@@ -0,0 +1,17 @@
+package pl.polsl.tpdia.fueldata.processor;
+
+/**
+ * @author mnalepa
+ *
+ */
+public class FuelDataProcessor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
