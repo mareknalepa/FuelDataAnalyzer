8bf3bedb7d7971fce353d52aafbaa1ed889619e5
Marek Nalepa <marek.nalepa@gmail.com>
2015-09-22 20:28:46 +0200

    Done loading data from CSV.

--- src/pl/polsl/tpdia/fueldata/analyzer/Analyzer.java
+++ src/pl/polsl/tpdia/fueldata/analyzer/Analyzer.java
@@ -1,13 +1,41 @@
 package pl.polsl.tpdia.fueldata.analyzer;
 
+import java.net.URISyntaxException;
+
+import pl.polsl.tpdia.fueldata.io.CsvLoader;
+import pl.polsl.tpdia.fueldata.model.DataHolder;
+
 public class Analyzer {
 
 	public static void main(String[] args) {
-		//TODO: load data
-		
-		//TODO: apply operators
-		
-		//TODO: save generated data
-	}
+		DataHolder dh = new DataHolder();
 
+		String nozzleMeasuresPath;
+		try {
+			nozzleMeasuresPath = Analyzer.class.getClassLoader()
+					.getResource("nozzleMeasures.log").toURI().getPath();
+		} catch (URISyntaxException e) {
+			nozzleMeasuresPath = null;
+		}
+		String refuelsPath;
+		try {
+			refuelsPath = Analyzer.class.getClassLoader()
+					.getResource("refuel.log").toURI().getPath();
+		} catch (URISyntaxException e) {
+			refuelsPath = null;
+		}
+		String tankMeasuresPath;
+		try {
+			tankMeasuresPath = Analyzer.class.getClassLoader()
+					.getResource("tankMeasures.log").toURI().getPath();
+		} catch (URISyntaxException e) {
+			tankMeasuresPath = null;
+		}
+
+		CsvLoader.load(dh, nozzleMeasuresPath, refuelsPath, tankMeasuresPath);
+
+		// TODO: apply operators
+
+		// TODO: save generated data
+	}
 }
--- src/pl/polsl/tpdia/fueldata/io/CsvLoader.java
+++ src/pl/polsl/tpdia/fueldata/io/CsvLoader.java
@@ -8,6 +8,7 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -77,6 +78,14 @@ public class CsvLoader {
 	private static NozzleMeasure parseNozzleMeasure(String[] parts) {
 		NozzleMeasure nm = new NozzleMeasure();
 
+		if (parts.length != 7) {
+			int length = parts.length;
+			parts = Arrays.copyOf(parts, 7);
+			for (int i = length; i < 7; ++i) {
+				parts[i] = "";
+			}
+		}
+
 		try {
 			nm.setTimestamp(df.parse(parts[0]));
 		} catch (ParseException e) {
@@ -85,8 +94,8 @@ public class CsvLoader {
 		nm.setLocationId(parts[1]);
 		nm.setId(Long.parseLong(parts[2]));
 		nm.setTankId(Long.parseLong(parts[3]));
-		nm.setCurrentCounter(Double.parseDouble(parts[4]));
-		nm.setTotalCounter(Double.parseDouble(parts[5]));
+		nm.setCurrentCounter(parseDouble(parts[4]));
+		nm.setTotalCounter(parseDouble(parts[5]));
 		int idle = Integer.parseInt(parts[6]);
 		nm.setNozzleIdle((idle == 1) ? true : false);
 
@@ -96,20 +105,36 @@ public class CsvLoader {
 	private static Refuel parseRefuel(String[] parts) {
 		Refuel r = new Refuel();
 
+		if (parts.length != 4) {
+			int length = parts.length;
+			parts = Arrays.copyOf(parts, 4);
+			for (int i = length; i < 4; ++i) {
+				parts[i] = "";
+			}
+		}
+
 		try {
 			r.setTimestamp(df.parse(parts[0]));
 		} catch (ParseException e) {
 			r.setTimestamp(new Date());
 		}
 		r.setTankId(Long.parseLong(parts[1]));
-		r.setFuelVolume(Double.parseDouble(parts[2]));
-		r.setRefuelSpeed(Double.parseDouble(parts[3]));
+		r.setFuelVolume(parseDouble(parts[2]));
+		r.setRefuelSpeed(parseDouble(parts[3]));
 
 		return r;
 	}
 
 	private static TankMeasure parseTankMeasure(String[] parts) {
 		TankMeasure tm = new TankMeasure();
+
+		if (parts.length != 9) {
+			int length = parts.length;
+			parts = Arrays.copyOf(parts, 9);
+			for (int i = length; i < 9; ++i) {
+				parts[i] = "";
+			}
+		}
 
 		try {
 			tm.setTimestamp(df.parse(parts[0]));
@@ -119,12 +144,22 @@ public class CsvLoader {
 		tm.setLocationId(parts[1]);
 		tm.setMeterId(parts[2]);
 		tm.setId(Long.parseLong(parts[3]));
-		tm.setFuelLevel(Double.parseDouble(parts[4]));
-		tm.setFuelVolume(Double.parseDouble(parts[5]));
-		tm.setFuelTemp(Double.parseDouble(parts[6]));
-		tm.setWaterLevel(Double.parseDouble(parts[7]));
-		tm.setWaterVolume(Double.parseDouble(parts[8]));
+		tm.setFuelLevel(parseDouble(parts[4]));
+		tm.setFuelVolume(parseDouble(parts[5]));
+		tm.setFuelTemp(parseDouble(parts[6]));
+		tm.setWaterLevel(parseDouble(parts[7]));
+		tm.setWaterVolume(parseDouble(parts[8]));
 
 		return tm;
+	}
+
+	private static double parseDouble(String str) {
+		double value;
+		try {
+			value = Double.parseDouble(str.replace(',', '.'));
+		} catch (NumberFormatException e) {
+			value = 0.0;
+		}
+		return value;
 	}
 }
