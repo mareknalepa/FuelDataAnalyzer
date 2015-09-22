db7181c9d63a4c2c50f89471584e15c8f56a41e1
Marek Nalepa <marek.nalepa@gmail.com>
2015-09-22 19:14:35 +0200

    CsvLoader.

--- src/pl/polsl/tpdia/fueldata/model/DataHolder.java
+++ src/pl/polsl/tpdia/fueldata/model/DataHolder.java
@@ -6,24 +6,29 @@ public class DataHolder {
 
 	private List<NozzleMeasure> nozzleMeasures;
 	private List<TankMeasure> tankMeasures;
-	private List<Refuel> refuel;
-	
+	private List<Refuel> refuels;
+
 	public List<NozzleMeasure> getNozzleMeasures() {
 		return nozzleMeasures;
 	}
+
 	public void setNozzleMeasures(List<NozzleMeasure> nozzleMeasures) {
 		this.nozzleMeasures = nozzleMeasures;
 	}
+
 	public List<TankMeasure> getTankMeasures() {
 		return tankMeasures;
 	}
+
 	public void setTankMeasures(List<TankMeasure> tankMeasures) {
 		this.tankMeasures = tankMeasures;
 	}
-	public List<Refuel> getRefuel() {
-		return refuel;
+
+	public List<Refuel> getRefuels() {
+		return refuels;
 	}
-	public void setRefuel(List<Refuel> refuel) {
-		this.refuel = refuel;
+
+	public void setRefuels(List<Refuel> refuels) {
+		this.refuels = refuels;
 	}
 }
